b1f5aeb0b3d742e76e12e953b22b254fe74d2c51
bliao2@uci.edu <ec2-user@ip-172-31-91-131.ec2.internal>
2022-11-06 18:21:41 +0000

    added script

---
+++ scripts/update_database.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.append("./..")
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+from backend.rent_scrapper import Arkadia_scrapper
+from backend.database_manager import DataBaseManager
+
+scarpper_west_arkadia = Arkadia_scrapper()
+db = DataBaseManager()
+df = scarpper_west_arkadia.main()
+db.push_newest_data(df)
