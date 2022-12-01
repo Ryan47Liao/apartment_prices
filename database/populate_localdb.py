fc4cc208f11b9abbda67c9f635bc00d339c38cae
Bowen Liao <bliao2@uci.edu>
2022-11-30 22:50:49 -0600

    refactored code to make sure folder existence

--- database/populate_localdb.py
+++ database/populate_localdb.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append("./..")
+sys.path.append("..")
 
 import pandas as pd
 from Logic.data_analyzer import DataAnalyzer
@@ -12,7 +12,7 @@ with da.db.pool.connect() as conn:
     sql = 'SELECT * FROM apartments.room_meta'
     df_meta = pd.read_sql(sql, conn)
     df_price = pd.read_sql('SELECT * FROM apartments.prices', conn)
-    with sqlite3.connect("../database/apartments.db") as conn1:
+    with sqlite3.connect("apartments.db") as conn1:
         df_meta.to_sql('room_meta', conn1, if_exists='append')
         print(pd.read_sql('SELECT * FROM room_meta', conn1).shape)
         df_price.to_sql('prices', conn1, if_exists='append')
