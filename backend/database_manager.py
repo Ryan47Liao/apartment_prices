647d711449ed20d6355d5c20c4acc57522618fea
Bowen Liao <bliao2@uci.edu>
2022-12-01 11:06:52 -0600

    fixed bug

--- backend/database_manager.py
+++ backend/database_manager.py
@@ -3,23 +3,25 @@ import sys
 sys.path.append("./..")
 
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 from sqlalchemy import create_engine
 import pandas as pd
 
+
 class DataBaseManager:
-    def __init__(self, config=None,local=True):
+    def __init__(self, config=None, local=True):
         if config is None:
             config = {
-            'user': "admin",
-            'password': "a19990721",
-            'host': "capstone.cfykpx21wk0f.us-east-2.rds.amazonaws.com",
-            'database': "apartments",
-            'port': '3306'}
+                'user': "admin",
+                'password': "a19990721",
+                'host': "capstone.cfykpx21wk0f.us-east-2.rds.amazonaws.com",
+                'database': "apartments",
+                'port': '3306'}
         self.config = config
         if local:
-            self.pool  = create_engine('sqlite:///../database/apartments.db', echo=True)
+            self.pool = create_engine('sqlite:///../database/apartments.db', echo=True)
         else:
             self.pool = create_engine(url=DataBaseManager.create_url(**self.config),
                                       pool_size=20, max_overflow=0
@@ -28,7 +30,6 @@ class DataBaseManager:
     @staticmethod
     def create_url(user, password, host, database, port):
         return f"mysql://{user}:{password}@{host}:{port}/{database}"
-
 
     def get_all_data(self):
         with self.pool.connect() as conn:
@@ -48,7 +49,7 @@ class DataBaseManager:
             df = pd.read_sql(sql, conn)
         return df
 
-    def push_newest_data(self,df):
+    def push_newest_data(self, df):
         pool = create_engine(url=DataBaseManager.create_url(**self.config),
                              pool_size=20, max_overflow=0,
                              )
@@ -58,7 +59,7 @@ class DataBaseManager:
             df.to_sql('prices', conn, if_exists='append', index=False)
             pass
 
-    def _update_meta(self,df, pool, apartment='Arkadia'):
+    def _update_meta(self, df, pool, apartment='Arkadia'):
         try:
             df_meta = pd.read_sql("select * from room_meta where apartment = '{}';".format(apartment), pool)
             existing_metas = df_meta.room_number.to_list()
@@ -67,8 +68,9 @@ class DataBaseManager:
         out_dfs = []
         for room_number, df_group in df.groupby('room_number'):
             if room_number not in existing_metas:
-                out_dfs.append(df_group[['room_number', 'apartment', 'Avaliable_date', 'Sq.Ft', 'Floor_Plan', 'num_bedroom',
-                                         'num_bathroom']])
+                out_dfs.append(
+                    df_group[['room_number', 'apartment', 'Avaliable_date', 'Sq.Ft', 'Floor_Plan', 'num_bedroom',
+                              'num_bathroom']])
         if len(out_dfs) > 0:
             with pool.connect() as conn:
                 pd.concat(out_dfs).to_sql('room_meta', conn, if_exists='append', index=False)
--- database/populate_localdb.py
+++ database/populate_localdb.py
@@ -7,13 +7,14 @@ from Logic.data_analyzer import DataAnalyzer
 # Localize
 import sqlite3
 
-da = DataAnalyzer()
+da = DataAnalyzer(local=False)
 with da.db.pool.connect() as conn:
     sql = 'SELECT * FROM apartments.room_meta_old'
     df_meta = pd.read_sql(sql, conn)
     df_price = pd.read_sql('SELECT * FROM apartments.prices_old', conn)
-    with sqlite3.connect("apartments.db") as conn1:
+    with sqlite3.connect("../database/apartments.db") as conn1:
         df_meta.to_sql('room_meta', conn1, if_exists='append')
-        print(pd.read_sql('SELECT * FROM room_meta', conn1).shape)
         df_price.to_sql('prices', conn1, if_exists='append')
-        print(pd.read_sql('SELECT * FROM prices', conn1).shape)
+    with sqlite3.connect("../database/apartments.db") as conn2:
+        print(pd.read_sql('SELECT * FROM room_meta', conn2).shape)
+        print(pd.read_sql('SELECT * FROM prices', conn2).shape)
