a638d08dc7727d0c8649cd539f7efb7e7e415fe2
Ryan Liao <rliao@drwholdings.com>
2023-08-04 13:22:35 -0500

    added dags

---
+++ dags/apartment_collect_data.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'cloud.user',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 8, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'apartment_data_collector',
+    default_args=default_args,
+    description='Populates database by scrapping an website',
+    schedule_interval=timedelta(hours=1),
+    catchup=False,
+) as dag:
+
+    t1 = DockerOperator(
+        task_id='docker_data_collector',
+        image='apartment_prices-data_collector',
+        api_version='auto',
+        auto_remove=True,
+        command="python scripts/update_database.py",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        mounts=[
+            Mount(source="/home/rliao/KingKong", target="/app/database", type="bind"),
+        ]
+    )
+
+# DAG Dependencies
+t1
